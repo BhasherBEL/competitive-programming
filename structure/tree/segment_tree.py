5271647fd8049ac6ad7036cfc4893434b23cdce1
Bhasher <github.it@bhasher.com>
2022-12-24 03:10:02 +0100

    Initial commit

---
+++ structure/tree/segment_tree.py
@@ -0,0 +1,41 @@
+class SegmentTree(object):
+    def __init__(self, array, fn, default=0):
+        self.size = len(array)
+        self.tree = [default] * self.size + array
+        self.fn = fn
+        self.default = default
+        
+        for i in range(self.size - 1, 0, -1):
+            self.tree[i] = fn(self.tree[2*i], self.tree[2*i+1])
+
+    def get(self, i, j):
+        i += self.size
+        j += self.size
+        res = self.default
+        while i < j:
+            if i&1:  # i%2 == 1
+                res = self.fn(res, self.tree[i])
+                i += 1 
+            if j&1:  # j%2 == 1
+                j -= 1
+                res = self.fn(res, self.tree[j])
+            i //= 2
+            j //= 2
+        return res
+
+    def update(self, p, v):
+        p += self.size
+        self.tree[p] = v
+        while p > 1:
+            p //= 2
+            self.tree[p] = self.fn(self.tree[p*2], self.tree[p*2+1])
+
+
+if __name__ == "__main__":
+    st = SegmentTree(
+        [1, 4, 8, 2, 1, 9, 4],
+        lambda a, b: a*b,
+        default=1
+    )
+
+    print(st.get(1, 4))
